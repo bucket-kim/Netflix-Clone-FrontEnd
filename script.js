53169ef27f94e03a9914576d15f59955d6c78a8e
Brian Kim <brian.kyounghoon.kim@gmail.com>
2021-01-08 18:50:56 +0900

    netflix original section done

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 
 window.onload = () => {
-  addMovies();
+  fetchMovie();
 };
 
+const fetchMovie = () => {
+  fetch("https://api.themoviedb.org/3/discover/movie?api_key=ea50658dff38598f9ee0db5955b8e2b4&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1")
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else { 
+      throw new Error("Error!")
+    }
+  })
+  .then((data) => {
+    addMovies(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+}
+
 // Add movies to the front end
-const addMovies = () => {
-  
+const addMovies = (movies) => {
   // add image to movies original
   let moviesEle = document.querySelector('.original__movies');
-  for (let i = 0; i < 10; i ++ ) {
-    moviesEle.innerHTML += '<img src="https://www.themoviedb.org/t/p/w1280/zU0htwkhNvBQdVSIKB9s6hgVeFK.jpg" alt="">'
+  for (movie of movies.results) {
+    let image = `<img src="https://image.tmdb.org/t/p/original${movie.poster_path}" alt="">`;
+    moviesEle.innerHTML += image;
   }
 }
+
+
+// ea50658dff38598f9ee0db5955b8e2b4
