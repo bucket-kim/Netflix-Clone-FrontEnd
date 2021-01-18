b2440ca4c25b54e7b1cc59a4493f4687ada31cdf
Brian Kim <brian.kyounghoon.kim@gmail.com>
2021-01-18 14:43:37 +0900

    logout function added

--- session.js
+++ session.js
@@ -5,10 +5,17 @@ const checkIfSignedIn = () => {
       location.href = "/";
     }
   } else {
+    // If i'm not currently logged in
+    // And trying to access to unauthorized page
     if (location.href != "http://127.0.0.1:5500/login.html") {
-      location.href = "/login";
+      location.href = "/login.html";
     }
   }
+};
+
+const logOut = () => {
+  localStorage.removeItem('token');
+  location.href = "/login.html";
 }
 
 checkIfSignedIn(); 
